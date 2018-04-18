38c675cb1c457439473bc7bffe4a3205fb1a06af
lakeisha-pogrzeba <lakeisha.pogrzeba@tr.com>
2018-04-17 23:30:30 -0400

    git scripts, sharedrc

---
+++ gitdelete.rb
@@ -0,0 +1,22 @@
+branches =  `git branch`
+arr = Array.new
+
+branches.each_line {|branch, index|
+  matches = branch.split(" ")
+  arr << matches.last
+}
+arr.each_with_index {|b, i|
+  puts "#{i}: #{b}"
+}
+branch_selected = ""
+selection = gets.chomp
+arr.each_with_index {|b, i|
+  if i == selection.to_i
+    branch_selected = b
+  end
+}
+puts 'you selected #{branch_selected}. press y to Delete branch'
+selection = gets.chomp
+if selection == 'y'
+puts `git branch -D #{branch_selected}`
+end
