836d42bfe641009a0e2f9f127a62577904193833
U6064854 <=>
2018-08-13 17:45:56 -0400

    started a file to keep track of setup things

---
+++ setup.sh
@@ -0,0 +1,36 @@
+install git
+install iterm2
+install spectacle
+install chrome
+isntall java jdk
+install mvn?
+login to github
+brew install ag
+brew install fzy
+brew install prettier
+brew isntall gradle
+ defaults write NSGlobalDomain InitialKeyRepeat -int 12
+ defaults write NSGlobalDomain KeyRepeat -int 1
+
+
+-install python
+-brew link python
+- sudo chown -R $(whoami) $(brew --prefix)/*
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+
+preferences->General>chrome default
+
+instal karabiner elemenets
+-brew install cmake
+- install boost
+- install xcode
+
+
+-install homebrew
+-brew install tmux
+- curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+https://github.com/sickill/vim-monokai.gIT
+
+
+link: tmux, gitconfig, init.vim, config.fish, 
